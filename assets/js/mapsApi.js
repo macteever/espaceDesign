40e751d66d7d054fc1875f3d2e88800c0ef5d24c
nmetivier <contact@nicolasmetivier.fr>
2018-10-20 16:24:10 +0200

    fin page contact

--- assets/js/mapsApi.js
+++ assets/js/mapsApi.js
@@ -2,45 +2,179 @@ function initMap() {
 
 				  // Specify features and elements to define styles.
 				  var styleArray = [
-				    {
-				      featureType: "all"
-				    },{
-				      featureType: "road.arterial",
-				      elementType: "geometry",
-				      stylers: [
-				        { hue: "#00ffee" },
-				        { saturation: 50 }
-				      ]
-				    },{
-				      featureType: "poi.business",
-				      elementType: "labels",
-				      stylers: [
-				        { visibility: "off" }
-				      ]
-				    }
-				  ];
+			    {
+			        "featureType": "administrative",
+			        "elementType": "labels.text.fill",
+			        "stylers": [
+			            {
+			                "color": "#6195a0"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "color": "#f2f2f2"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#ffffff"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi.park",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#e6f3d6"
+			            },
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "saturation": -100
+			            },
+			            {
+			                "lightness": 45
+			            },
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#f4d2c5"
+			            },
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "labels.text",
+			        "stylers": [
+			            {
+			                "color": "#4e4e4e"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.arterial",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#f4f4f4"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.arterial",
+			        "elementType": "labels.text.fill",
+			        "stylers": [
+			            {
+			                "color": "#787878"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.arterial",
+			        "elementType": "labels.icon",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "elementType": "all",
+			        "stylers": [
+			            {
+			                "color": "#eaf6f8"
+			            },
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#eaf6f8"
+			            }
+			        ]
+			    }
+			];
 
 				  // Create a map object and specify the DOM element for display.
 				  var map = new google.maps.Map(document.getElementById('map'), {
-				    center: {lat: 46.702216, lng: 0.160131},
+				    center: {lat: 44.853219, lng: -0.568271},
 				    scrollwheel: false,
 				    // Apply the map style array to the map.
 				    styles: styleArray,
-				    zoom: 10
+				    zoom: 15
 				  });
 
-
-				  var myLatLng = {lat: 46.702216, lng: 0.160131};
+				  var myLatLng = {lat: 44.853219, lng: -0.568271};
 
 				  var marker = new google.maps.Marker({
 				    map: map,
-				    position: myLatLng
+				    position: myLatLng,
+						  icon :'../wp-content/themes/espaceDesign/assets/img/mapmarker.png'
 				  });
 
-				  
+
 				  var secretMessage = 'No place like home';
 
-				
+
 				  var infowindow = new google.maps.InfoWindow({
 				      content: secretMessage
 				    });
@@ -52,10 +186,3 @@ function initMap() {
 				    });
 
 				}
-				  
-
-
-	
-				
-
-
--- templates/template-contact.php
+++ templates/template-contact.php
@@ -9,7 +9,7 @@
 					<?php echo do_shortcode('[contact-form-7 id="150" title="page-contact"]'); ?>
 				</div>
 				<div class="col-xl-5 col-lg-5 col-md-12 col-12 d-flex flex-column justify-content-around">
-					<h3 class="fs-20">Nous trouver</h3>
+					<h3 class="fs-20 fw-300">Nous trouver</h3>
 					<?php
 					if( have_rows('coord_horaire') ):
 					    while ( have_rows('coord_horaire') ) : the_row();
@@ -27,10 +27,14 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
+		</div>`
+		<div class="container-fluid mt-80">
+			<div class="row before-map">
+			</div>
+			<div class="row map">
 				<div id="map"></div>
 			</div>
-	    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYM2FZ6PBFvla3XFMkE6xALHBw2KPY3LY&callback=initMap"></script>
+			<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYM2FZ6PBFvla3XFMkE6xALHBw2KPY3LY&callback=initMap"></script>
 		</div>
 	</main>
 <!-- /container-fluid -->
